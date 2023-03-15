97fd8f13c307b5fa6d9be6b35f27fc70e42abb49
Abir-Al-Arafat <antorabir@gmail.com>
2023-03-15 12:39:29 +0600

    pound to kg converter added

--- Weight Converter/script.js
+++ Weight Converter/script.js
@@ -1,11 +1,21 @@
 // output
 const outputPounds = document.getElementById("outputPounds");
+const outputKG = document.getElementById("outputKG");
 
-
-function weightConverter(kg){
+function convertKGToPound(kg){
+    // converting
     let pound = kg*2.2046;
     // rounding the value
     pound = pound.toFixed(0);
     // displaying the result
     outputPounds.innerHTML = pound;
+}
+
+function convertPoundToKG(pound){
+    // converting
+    let kg = pound / 2.2046;
+    // rounding
+    kg = kg.toFixed(0);
+    // displaying the result
+    outputKG.innerHTML = kg; 
 }
